6b6ed8a329970b823b3474adbb9bf97ca66f6c68
Mattias Festin <mail@mattiasfest.in>
2021-10-02 16:10:26 +0200

    dev

--- src/camera.rs
+++ src/camera.rs
@@ -7,10 +7,6 @@ use bevy_frustum_culling::FrustumCulling;
 pub fn setup_camera(mut commands: Commands) {
     commands.spawn()
     .insert_bundle(PerspectiveCameraBundle {
-        camera: Camera {
-            name: Some("PlayerCamera".to_string()),
-            ..Default::default()
-        },
         transform: Transform::from_translation(Vec3::new(0.0, 0.0, 20.0)).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
     })
